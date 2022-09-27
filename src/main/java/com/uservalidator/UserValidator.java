d5725cd139b54360a70dfc1f8e7ab48dc6408d6e
shaleshdutt1006 <shaleshdutt1006@gmail.com>
2022-09-27 13:56:30 +0530

    Added : Validating Password with at least one upper case and eight minimum characters

--- src/main/java/com/uservalidator/UserValidator.java
+++ src/main/java/com/uservalidator/UserValidator.java
@@ -4,6 +4,9 @@ import java.util.regex.Pattern;
 
 public class UserValidator {
 
+    /*
+    Validating Password with at least one upper case and eight minimum characters
+     */
     private static final String Password_Pattern = "(?=.*[A-Z])" + ".{8,}";
 
     public boolean vaildatePassword(String Password) {
