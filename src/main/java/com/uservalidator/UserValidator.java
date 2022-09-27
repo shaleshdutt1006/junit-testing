4b816f525a8890610232ace77d93090e846b979b
shaleshdutt1006 <shaleshdutt1006@gmail.com>
2022-09-27 16:18:53 +0530

    Added : Validating all information of the user like fname,lname,mobile number,email and password

--- src/main/java/com/uservalidator/UserValidator.java
+++ src/main/java/com/uservalidator/UserValidator.java
@@ -1,31 +1,64 @@
 package com.uservalidator;
 
-import java.util.List;
 import java.util.regex.Pattern;
 
 public class UserValidator {
+    private static final String First_Name_Pattern = "^[A-Z]{1}[a-z]{2,}$";
 
-     /*
-     Describing Pattern of email and compile it
+    /*
+    Method to validate First Name Of the User with first letter Capital and at least three Character
      */
+    public boolean validateFirstName(String fname) {
+        Pattern pattern = Pattern.compile(First_Name_Pattern);
+        return pattern.matcher(fname).matches();
+    }
 
-    String emailRegex = "^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*" + "@([a-zA-Z0-9][-]?)+[.][a-zA-Z]{2,4}([.]{2,4})?$";
-    Pattern pattern = Pattern.compile(emailRegex);
+    private static final String Last_Name_Pattern = "^[A-Z]{1,}[a-z]{3,}+\s+[A-Z]{1,}[a-z]{2,}$";
 
-    public boolean vaildateEmail(List<String> arrayList) {
-        boolean flag = true;
-        for (int i = 0; i < arrayList.size(); i++) {
+    /*
+    Method to validate Last Name Of the User with first letter Capital and at least three Character
+     */
+    public boolean validateLastName(String lname) {
+        Pattern pattern = Pattern.compile(Last_Name_Pattern);
+        return pattern.matcher(lname).matches();
+    }
 
-         /*
-         using for-loop checking every email whether it matches the defined pattern or not
-          */
-            if (pattern.matcher(arrayList.get(i)).matches()) {
-                System.out.println("valid");
-            } else {
-                System.out.println("invalid");
-                flag = false;
-            }
+    private static final String MobileNumber_Pattern = "^91{1}" + "\s" + "[0-9]{10,}$";
+
+    /*
+    Method to validate mobile number with country code and 10 digits
+    */
+    public boolean vaildateMobileNumber(String MobileNumber) {
+        Pattern pattern = Pattern.compile(MobileNumber_Pattern);
+        return pattern.matcher(MobileNumber).matches();
+    }
+
+    private static final String Password_Pattern = "(?=.*[A-Z])" + "(?=.*[0-9])" + "(?=.*[@#$%^&+=])" + "(?=\\S+$)" + ".{8,}";
+
+    /*
+     Validating password which has at least 8 character one uppercase letter and one numeric number and
+      at least one special character and no whitespace allowed
+     */
+    public boolean vaildatePassword(String Password) {
+        Pattern pattern = Pattern.compile(Password_Pattern);
+
+        if (pattern.matcher(Password).matches()) {
+            System.out.println("Password is set");
+            return true;
+        } else {
+            System.out.println("Please Enter valid input Password is not Set");
+            return false;
         }
-        return flag;
+    }
+
+    private static final String Email_Pattern = "^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*" + "@([a-zA-Z0-9][-]?)+[.][a-zA-Z]{2,4}([.]{2,4})?$";
+
+    /*
+    Validating email with Special Character and .com and some numeric words
+     */
+    public boolean vaildateEmail(String email) {
+        Pattern pattern = Pattern.compile(Email_Pattern);
+        return pattern.matcher(email).matches();
+
     }
 }
--- src/test/java/com/uservalidator/UserValidatorTest.java
+++ src/test/java/com/uservalidator/UserValidatorTest.java
@@ -3,38 +3,39 @@ package com.uservalidator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-
 public class UserValidatorTest {
-    /*
-       Taking an Arraylist to check Various email whether it is correct or not and adding emails in the list
-  */
-    ArrayList<String> arrayList = new ArrayList<>();
+    @Test
+    public void givenFirstName_WhenProper_ShouldReturnTrue() {
+        UserValidator validator = new UserValidator();
+        boolean result = validator.validateFirstName("Shalesh");
+        Assertions.assertEquals(true, result);
+    }
 
-    public void setArrayList() {
-    /*
-    Method to add elements in arraylist to check for patterns
-    */
-        arrayList.add("abc111@abc.com");
-        arrayList.add("abc-100@abc.com");
-        arrayList.add("abc.100@yahoo.com");
-        arrayList.add("abc+100@gmail.com");
-        arrayList.add("abc@1.com");
-        arrayList.add("abc-100@yahoo.com");
-        arrayList.add("abc_pqr@gmail.com");
-        arrayList.add("abc.100@abc.com.au");
-        arrayList.add("abc()*@gmail.com");
-        arrayList.add("abc@%*.com");
-        arrayList.add("abc@abc@gmail.com");
-        arrayList.add("abc@gmail.com.1a");
-        arrayList.add(".abc@abc.com");
+    @Test
+    public void givenLastName_WhenProper_ShouldReturnTrue() {
+        UserValidator validator = new UserValidator();
+        boolean result = validator.validateLastName("Shalesh Dutt");
+        Assertions.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenMobileNumber_WhenProperFormat_ShouldReturnTrue() {
+        UserValidator validator = new UserValidator();
+        boolean result = validator.vaildateMobileNumber("91 9425109980");
+        Assertions.assertEquals(true, result);
     }
 
     @Test
     public void givenPassword_WhenProperFormat_ShouldReturnTrue() {
         UserValidator validator = new UserValidator();
-        setArrayList();
-        boolean result = validator.vaildateEmail(arrayList);
+        boolean result = validator.vaildatePassword("Password123#");
+        Assertions.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenEmail_WhenProperFormat_ShouldReturnTrue() {
+        UserValidator validator = new UserValidator();
+        boolean result = validator.vaildateEmail("abc.100@yahoo.com");
         Assertions.assertEquals(true, result);
     }
 }
