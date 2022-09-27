7a0789506608e1db7f0fda6229d86e0fee32b806
shaleshdutt1006 <shaleshdutt1006@gmail.com>
2022-09-27 20:28:23 +0530

    Added : Refactored the code and throw custom exception for fname,lname,mobile number,email and password

--- src/main/java/com/uservalidator/UserValidator.java
+++ src/main/java/com/uservalidator/UserValidator.java
@@ -7,58 +7,71 @@ public class UserValidator {
 
     /*
     Method to validate First Name Of the User with first letter Capital and at least three Character
+     and throwing Custom Exception.
      */
-    public boolean validateFirstName(String fname) {
+    public boolean validateFirstName(String fname) throws CustomException {
         Pattern pattern = Pattern.compile(First_Name_Pattern);
-        return pattern.matcher(fname).matches();
+
+        if (pattern.matcher(fname).matches()) {
+            return true;
+        }
+        throw new CustomException("Please Enter proper first name");
     }
 
     private static final String Last_Name_Pattern = "^[A-Z]{1,}[a-z]{3,}+\s+[A-Z]{1,}[a-z]{2,}$";
 
     /*
     Method to validate Last Name Of the User with first letter Capital and at least three Character
+     and throwing Custom Exception.
      */
-    public boolean validateLastName(String lname) {
+    public boolean validateLastName(String lname) throws CustomException {
         Pattern pattern = Pattern.compile(Last_Name_Pattern);
-        return pattern.matcher(lname).matches();
+        if (pattern.matcher(lname).matches()) {
+            return true;
+        }
+        throw new CustomException("Please Enter proper Last name");
     }
 
     private static final String MobileNumber_Pattern = "^91{1}" + "\s" + "[0-9]{10,}$";
 
     /*
-    Method to validate mobile number with country code and 10 digits
+    Method to validate mobile number with country code and 10 digits and throwing Custom Exception.
     */
-    public boolean vaildateMobileNumber(String MobileNumber) {
+    public boolean vaildateMobileNumber(String MobileNumber) throws CustomException {
         Pattern pattern = Pattern.compile(MobileNumber_Pattern);
-        return pattern.matcher(MobileNumber).matches();
+        if (pattern.matcher(MobileNumber).matches()) {
+            return true;
+        }
+        throw new CustomException("Please Enter proper Mobile Number");
     }
 
     private static final String Password_Pattern = "(?=.*[A-Z])" + "(?=.*[0-9])" + "(?=.*[@#$%^&+=])" + "(?=\\S+$)" + ".{8,}";
 
     /*
      Validating password which has at least 8 character one uppercase letter and one numeric number and
-      at least one special character and no whitespace allowed
+      at least one special character and no whitespace allowed and throwing Custom Exception.
      */
-    public boolean vaildatePassword(String Password) {
+    public boolean vaildatePassword(String Password) throws CustomException {
         Pattern pattern = Pattern.compile(Password_Pattern);
 
         if (pattern.matcher(Password).matches()) {
-            System.out.println("Password is set");
             return true;
-        } else {
-            System.out.println("Please Enter valid input Password is not Set");
-            return false;
         }
+        throw new CustomException("Please Enter proper Password");
     }
 
     private static final String Email_Pattern = "^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*" + "@([a-zA-Z0-9][-]?)+[.][a-zA-Z]{2,4}([.]{2,4})?$";
 
     /*
-    Validating email with Special Character and .com and some numeric words
+    Validating email with Special Character and .com and some numeric words and throwing Custom Exception
      */
-    public boolean vaildateEmail(String email) {
+    public boolean vaildateEmail(String email) throws CustomException {
         Pattern pattern = Pattern.compile(Email_Pattern);
-        return pattern.matcher(email).matches();
+        if (pattern.matcher(email).matches()) {
+            return true;
+        }
+        throw new CustomException("Please Enter proper Email Address");
+
 
     }
 }
--- src/test/java/com/uservalidator/UserValidatorTest.java
+++ src/test/java/com/uservalidator/UserValidatorTest.java
@@ -5,38 +5,59 @@ import org.junit.jupiter.api.Test;
 
 public class UserValidatorTest {
     @Test
-    public void givenFirstName_WhenProper_ShouldReturnTrue() {
+    public void FirstName_WhenNotProperFormat_ShouldThrowException() throws CustomException {
         UserValidator validator = new UserValidator();
-        boolean result = validator.validateFirstName("Shalesh");
-        Assertions.assertEquals(true, result);
+        try {
+            boolean result = validator.validateFirstName("Shal@sh");
+            Assertions.assertEquals(true, result);
+        } catch (CustomException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     @Test
-    public void givenLastName_WhenProper_ShouldReturnTrue() {
+    public void givenLastName_WhenNotProperFormat_ShouldThrowException() {
         UserValidator validator = new UserValidator();
-        boolean result = validator.validateLastName("Shalesh Dutt");
-        Assertions.assertEquals(true, result);
+        try {
+            boolean result = validator.validateLastName("Shalesh dutt");
+            Assertions.assertEquals(true, result);
+        } catch (CustomException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     @Test
-    public void givenMobileNumber_WhenProperFormat_ShouldReturnTrue() {
+    public void givenMobileNumber_WhenNotProperFormat_ShouldThrowException() {
         UserValidator validator = new UserValidator();
-        boolean result = validator.vaildateMobileNumber("91 9425109980");
-        Assertions.assertEquals(true, result);
+        try {
+
+            boolean result = validator.vaildateMobileNumber("91 94251099");
+            Assertions.assertEquals(true, result);
+        } catch (CustomException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     @Test
-    public void givenPassword_WhenProperFormat_ShouldReturnTrue() {
+    public void givenPassword_WhenNotProperFormat_ShouldThrowException() {
         UserValidator validator = new UserValidator();
-        boolean result = validator.vaildatePassword("Password123#");
-        Assertions.assertEquals(true, result);
+        try {
+            boolean result = validator.vaildatePassword("Password123");
+            Assertions.assertEquals(true, result);
+        } catch (CustomException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     @Test
-    public void givenEmail_WhenProperFormat_ShouldReturnTrue() {
+    public void givenEmail_WhenNotProper_ShouldThrowException() {
         UserValidator validator = new UserValidator();
-        boolean result = validator.vaildateEmail("abc.100@yahoo.com");
-        Assertions.assertEquals(true, result);
+        try {
+            boolean result = validator.vaildateEmail("abc.100yahoo.com");
+            Assertions.assertEquals(true, result);
+        } catch (CustomException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
 
