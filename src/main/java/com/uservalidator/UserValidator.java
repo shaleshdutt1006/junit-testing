6102ac59e1a482ad2bdbfd3836cf20fac2eaef50
shaleshdutt1006 <shaleshdutt1006@gmail.com>
2022-09-27 10:38:58 +0530

    Added : Welcome message

---
+++ src/main/java/com/uservalidator/UserValidator.java
@@ -0,0 +1,9 @@
+package com.uservalidator;
+
+import java.util.regex.Pattern;
+
+public class UserValidator {
+
+
+
+}
---
+++ src/test/java/com/uservalidator/UserValidatorTest.java
@@ -0,0 +1,10 @@
+package com.uservalidator;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class UserValidatorTest {
+
+
+
+}
