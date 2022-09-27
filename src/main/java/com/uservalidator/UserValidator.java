7d583dda4719b8d985e36b1e519ebbc7165ad881
shaleshdutt1006 <shaleshdutt1006@gmail.com>
2022-09-27 14:39:56 +0530

    Added : Validating Password with at least one UpperCase,one Numeric and one Special character

--- src/main/java/com/uservalidator/UserValidator.java
+++ src/main/java/com/uservalidator/UserValidator.java
@@ -5,11 +5,12 @@ import java.util.regex.Pattern;
 public class UserValidator {
 
     /**
-     * Setting password which has at least 8 character one uppercase letter and one numeric number
+     * Setting password which has at least 8 character one uppercase letter and one numeric number and
+     * at least one special character and no whitespace allowed
      *
      * @param args
      */
-    private static final String Password_Pattern = "(?=.*[A-Z])" + "(?=.*[0-9])" + ".{8,}";
+    private static final String Password_Pattern = "(?=.*[A-Z])" + "(?=.*[0-9])" + "(?=.*[@#$%^&+=])" + "(?=\\S+$)" + ".{8,}";
 
     public boolean vaildatePassword(String Password) {
         Pattern pattern = Pattern.compile(Password_Pattern);
--- src/test/java/com/uservalidator/UserValidatorTest.java
+++ src/test/java/com/uservalidator/UserValidatorTest.java
@@ -8,14 +8,14 @@ public class UserValidatorTest {
     @Test
     public void givenPassword_WhenProperFormat_ShouldReturnTrue() {
         UserValidator validator = new UserValidator();
-        boolean result = validator.vaildatePassword("Password123");
+        boolean result = validator.vaildatePassword("Password123#");
         Assertions.assertEquals(true, result);
     }
 
     @Test
-    public void givenPassword_WithoutNumericNumber_ShouldReturnFalse() {
+    public void givenPassword_WithoutSpecialChar_ShouldReturnFalse() {
         UserValidator validator = new UserValidator();
-        boolean result = validator.vaildatePassword("passWord");
+        boolean result = validator.vaildatePassword("passWord12");
         Assertions.assertEquals(true, result);
     }
 
