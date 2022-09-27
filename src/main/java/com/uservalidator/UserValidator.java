21967db708e356231556e4ff8f05bdb0a7b7c934
shaleshdutt1006 <shaleshdutt1006@gmail.com>
2022-09-27 10:45:48 +0530

    Added : Validating First Name with at least three minimum characters

--- src/main/java/com/uservalidator/UserValidator.java
+++ src/main/java/com/uservalidator/UserValidator.java
@@ -4,6 +4,12 @@ import java.util.regex.Pattern;
 
 public class UserValidator {
 
+    private static final String First_Name_Pattern = "^[A-Z]{1}[a-z]{2,}$";
+
+    public boolean validateFirstName(String fname) {
+        Pattern pattern = Pattern.compile(First_Name_Pattern);
+        return pattern.matcher(fname).matches();
+    }
 
 
 }
--- src/test/java/com/uservalidator/UserValidatorTest.java
+++ src/test/java/com/uservalidator/UserValidatorTest.java
@@ -5,6 +5,28 @@ import org.junit.jupiter.api.Assertions;
 
 public class UserValidatorTest {
 
+    @Test
+    public void givenFirstName_WhenProper_ShouldReturnTrue() {
+        UserValidator validator = new UserValidator();
+        boolean result = validator.validateFirstName("Shalesh");
+        Assertions.assertEquals(true, result);
+    }
 
+    @Test
+    public void givenFirstName_WhenShort_ShouldReturnFalse() {
+        UserValidator validator = new UserValidator();
+        boolean result = validator.validateFirstName("sh");
+        Assertions.assertFalse(result);
+    }
 
+    @Test
+    public void givenFirstName_WhenWithSpChars_ShouldReturnfalse() {
+        UserValidator validator = new UserValidator();
+        boolean result = validator.validateFirstName("Sha@lesh");
+        Assertions.assertFalse(result);
+    }
 }
+
+
+
+
